e859a298e2680e3872db93a12dc001530518ea3c
Bhagya Wijenayake <63334319+Bhagyawijenayake@users.noreply.github.com>
2023-06-19 11:32:43 +0530

    add readmore

--- src/Tour.jsx
+++ src/Tour.jsx
@@ -1,19 +1,35 @@
-import React from 'react'
+import React, { useState } from "react";
 
-const Tour = ({id, image,info,name,price,removeTour}) => {
+const Tour = ({ id, image, info, name, price, removeTour }) => {
+  const [readMore, setReadmore] = useState(false);
+
   return (
-      <article className='single-tour'>
-          <img src={image} alt={name} className='img' />
-          <span className="tour-price">${price}</span>
-          <div className="tour-info">
-              <h5>name</h5>
-        <p>{info}</p>
-        <button type='button' className='btn btn-block delete-btn' onClick={()=>removeTour(id)}>not interested</button>
+    <article className="single-tour">
+      <img src={image} alt={name} className="img" />
+      <span className="tour-price">${price}</span>
+      <div className="tour-info">
+        <h5>name</h5>
+        <p>
+          {readMore ? info : `${info.substring(0, 200)}...`}
+          <button
+            type="button"
+            className="info-btn"
+            onClick={() => setReadmore(!readMore)}
+          >
+            {readMore ? "show less" : "read more"}
+          </button>
+        </p>
 
-
-          </div>
+        <button
+          type="button"
+          className="btn btn-block delete-btn"
+          onClick={() => removeTour(id)}
+        >
+          not interested
+        </button>
+      </div>
     </article>
-  )
-}
+  );
+};
 
-export default Tour
+export default Tour;
